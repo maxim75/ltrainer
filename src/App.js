2716a9a336312365d3b90e2c2b74904d546d3555
Maksym Kozlenko <max@kozlenko.info>
2016-11-23 16:34:47 +1100

    init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import lodash from 'lodash';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <p>ZZ {logo}</p>
+
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
